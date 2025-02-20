09db23181c4346e31dbf01eec62048183b8e5523
rajchovatiya <rajchovatiya783@gmail.com>
2025-02-20 12:51:22 +0530

    add servicecart cod

--- scriptpage/script.js
+++ scriptpage/script.js
@@ -1,22 +1,25 @@
-$(function () {
-    function gsapanimesn() {
-        gsap.from(".conten_img h1,.conten_img p", {
-            x: -700,
-            duration: 1.5,
-            delay: 0.16,
-            opacity: 0
+    gsap.from(".conten_img h1,.conten_img p", {
+        x: -700,
+        duration: 1.5,
+        delay: 0.16,
+        opacity: 0
 
-        })
+    })
 
-        gsap.from(".conten_img button", {
-            x: -500,
-            duration: 1.5,
-            delay: 0.14,
-            opacity: 0
+    gsap.from(".conten_img button", {
+        x: -500,
+        duration: 1.5,
+        delay: 0.14,
+        opacity: 0
 
-        })
+    })
+    gsap.from(".service_box", {
+        opacity: 0,
+        y: 200,
+        duration: 1.2,
+        delay: 0.14,
+        stagger:0.3
 
-    }
-    gsapanimesn()
-})
+    })
+
 
